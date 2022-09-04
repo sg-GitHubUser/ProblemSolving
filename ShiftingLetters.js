b7279de0c71a3867852b9cffe7540f9016cd15b6
sg-GitHubUser <88315860+sg-GitHubUser@users.noreply.github.com>
2022-09-04 17:22:38 +0530

    Create ShiftingLetters.js

---
+++ ShiftingLetters.js
@@ -0,0 +1,44 @@
+/*You are given a string s of lowercase English letters and an integer array shifts of the same length.
+Call the shift() of a letter, the next letter in the alphabet, (wrapping around so that 'z' becomes 'a').
+For example, shift('a') = 'b', shift('t') = 'u', and shift('z') = 'a'.
+Now for each shifts[i] = x, we want to shift the first i + 1 letters of s, x times.
+Return the final string after all such shifts to s are applied.
+
+Example 1:
+
+Input: s = "abc", shifts = [3,5,9]
+Output: "rpl"
+Explanation: We start with "abc".
+After shifting the first 1 letters of s by 3, we have "dbc".
+After shifting the first 2 letters of s by 5, we have "igc".
+After shifting the first 3 letters of s by 9, we have "rpl", the answer.
+Example 2:
+
+Input: s = "aaa", shifts = [1,2,3]
+Output: "gfd"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+shifts.length == s.length
+0 <= shifts[i] <= 109*/
+
+/*** Program Part ***/
+
+var s="abc", shifts=[3,5,9];
+const len = shifts.length;
+var res=s.split('');
+for(var i=0;i<len;i++)
+{
+    for(var j=0; j<=i;j++)
+    {
+         
+        res[j]=String.fromCharCode(s.charCodeAt(j)+shifts[i]);
+       
+        s=res.join('');
+    }
+    
+}
+console.log(s)
